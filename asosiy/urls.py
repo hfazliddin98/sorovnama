609f505c4ee2d43d5b048369716e290764487195
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-11-07 15:22:41 +0500

    seting

--- asosiy/urls.py
+++ asosiy/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('', include('users.urls')),
     path('baho/', include('baho.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
