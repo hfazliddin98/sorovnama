d691c897b1645bc22785f5ebdf34bb0c2f04525e
hfazliddin98 <hatamovfazliddin505@gmail.com>
2023-11-08 22:01:32 +0500

    admin

--- baho/admin.py
+++ baho/admin.py
@@ -4,23 +4,23 @@ from .models import Fan, Turi, Oqtuvchi, Sorovnoma, Baza
 
 @admin.register(Sorovnoma)
 class SorovnomaAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['id', 'oqtuvchi_id', 'baho', 'baza']
 
 
 @admin.register(Oqtuvchi)
 class OqtuvchiAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['id', 'kurs_id', 'fan_id', 'tur_id']
 
 @admin.register(Fan)
 class FanAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['id','kurs_id', 'name']
 
 
 @admin.register(Turi)
 class TuriAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['id', 'fan_id', 'name']
 
 @admin.register(Baza)
 class BazaAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ['id', 'kurs', 'fan', 'tur', 'oqtuvchi', 'baho']
 
